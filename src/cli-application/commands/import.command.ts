5a21506475c3235e0db2f59c455dabf30700babf
Gleb <kochergin.gleb2016@yandex.ru>
2023-10-31 23:05:12 +0500

    fix and add test

--- src/cli-application/commands/import.command.ts
+++ src/cli-application/commands/import.command.ts
@@ -1,28 +1,19 @@
 import { TSVFileReader } from '../file-reader/file-reader.js';
 import { CliCommandInterface } from './cli-command.interface';
-import chalk from 'chalk';
 
 export default class ImportCommand implements CliCommandInterface {
   public readonly name = '--import';
 
   public execute(filename: string): void {
-    if (!filename) {
-      console.log(chalk.red('Filename required'));
-      return;
-    }
-
     const fileReader = new TSVFileReader(filename.trim());
-
     try {
       fileReader.read();
       console.log(fileReader.toArray());
-    } catch (err) {
-
-      if (!(err instanceof Error)) {
-        throw err;
+    } catch (exc) {
+      if (!(exc instanceof Error)) {
+        throw exc;
       }
-
-      console.log(err.message);
+      console.log(exc.message);
     }
   }
 }
--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import CLIApplication from './cli-application/cli.js';
-import HelpCommand from './cli-application/commands/help.command';
-import ImportCommand from './cli-application/commands/import.command';
-import VersionCommand from './cli-application/commands/version.command';
+import HelpCommand from './cli-application/commands/help.command.js';
+import ImportCommand from './cli-application/commands/import.command.js';
+import VersionCommand from './cli-application/commands/version.command.js';
 
 const cliApp = new CLIApplication();
 cliApp.registerCommands([new HelpCommand, new VersionCommand, new ImportCommand]);
